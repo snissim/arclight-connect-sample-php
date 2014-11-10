789ec08b5aff25e70558ecdf98821e8aadebcd76
snissim <samnissim@gmail.com>
2014-11-10 11:56:41 -0600

    removed device key

--- config/settings.php
+++ config/settings.php
@@ -4,7 +4,7 @@ define("BASEURL",URL . "/1.3/pmi");
 define("ACCESS_CONTROL_WSDL", BASEURL . "/AccessControlService.svc?singleWsdl");
 define("REFERENCE_WSDL", BASEURL .  "/ReferenceService.svc?singleWsdl");
 define("CONSUMER_WSDL", BASEURL . "/ConsumerService.svc?singleWsdl");
-define("DEVICE_KEY", "5DE6877A-FEB3-4C50-8CC0-458140ABFBBB");
+define("DEVICE_KEY", "00000000-0000-0000-0000-000000000000");
 define("MARKET_CODE", "AR");
 define("DIRECT_MAIL_DELIVERY_STATUS_CODE", "4M");
 define("EMAIL_DELIVERABILITY_STATUS_CODE", "3D");
