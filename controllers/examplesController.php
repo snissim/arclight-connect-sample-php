41ed69e1eb6db69d78650f0fa634d1bbc6ca3fa9
Derrick Shaw <derrick.shaw@arcww.com>
2014-11-10 06:49:34 -0600

    Updates to fix Registration profile

--- controllers/examplesController.php
+++ controllers/examplesController.php
@@ -2,29 +2,31 @@
 
 
 $app->map('/registration', function () use  ($twig, $app)  {
-    $responsAttributes = Array(
-        "FirstName" => $app->request->post('FirstName'),
-        "MiddleName" => $app->request->post('MiddleName'),
-        "PaternalLastName" => $app->request->post('PaternalLastName'),
-        "SourceCity" => $app->request->post('SourceCity'),
-        "SourceProvince" => $app->request->post('SourceProvince'),
-        "DirectMailPostalCode" => $app->request->post('DirectMailPostalCode'),
-        "GenderCode" => $app->request->post('GenderCode'),
-        'GIIDType1Code' => $app->request->post('GIIDType1Code'),
-        'EmailAddress' => $app->request->post('EmailAddress'),
-        "DateOfBirth" => $app->request->post('DateOfBirth'),
-        "ResponseDate" => $app->request->post('ResponseDate'),
-        "CampaignCode" => $app->request->post('CampaignCode'),
-        "PhaseCode" => $app->request->post('PhaseCode'),
-        "AudienceCode" => $app->request->post('AudienceCode'),
-        "MediaCategoryCode" => $app->request->post('MediaCategoryCode'),
-        "PieceCategoryCode" => $app->request->post('PieceCategoryCode'),
-        "PieceCode" => $app->request->post('PieceCode'),
-        "KeyDate" => $app->request->post('KeyDate'),
-        "DirectMailDeliveryStatusCode" => DIRECT_MAIL_DELIVERY_STATUS_CODE,
-        "EmailDeliverabilityStatusCode" => EMAIL_DELIVERABILITY_STATUS_CODE,
-        "HasAcceptedLegalTermsCode" => $app->request->post('HasAcceptedLegalTermsCode') != "Y"? "N":"Y",
-        "ProfileCode" => $app->request->post('ProfileCode'));
+    $resItems = array();
+    $resItems [0] = array("Name" => "FirstName", "Value" => $app->request->post('FirstName'));
+    $resItems [1] = array("Name" => "MiddleName", "Value" => $app->request->post('MiddleName'));
+    $resItems [2] = array("Name" => "PaternalLastName", "Value" => $app->request->post('PaternalLastName'));
+    $resItems [3] = array("Name" => "SourceCity", "Value" => $app->request->post('SourceCity'));
+    $resItems [4] = array("Name" => "SourceProvince", "Value" => $app->request->post('SourceProvince'));
+    $resItems [5] = array("Name" => "DirectMailPostalCode", "Value" => $app->request->post('DirectMailPostalCode'));
+    $resItems [6] = array("Name" => "GenderCode", "Value" => $app->request->post('GenderCode'));
+    $resItems [7] = array("Name" => "GIIDType1Code", "Value" => $app->request->post('GIIDType1Code'));
+    $resItems [8] = array("Name" => "EmailAddress", "Value" => $app->request->post('EmailAddress'));
+    $resItems [9] = array("Name" => "DateOfBirth", "Value" => $app->request->post('DateOfBirth'));
+    $resItems [10] = array("Name" => "ResponseDate", "Value" => $app->request->post('ResponseDate'));
+    $resItems [11] = array("Name" => "CampaignCode", "Value" => $app->request->post('CampaignCode'));
+    $resItems [12] = array("Name" => "PhaseCode", "Value" => $app->request->post('PhaseCode'));
+    $resItems [13] = array("Name" => "AudienceCode", "Value" => $app->request->post('AudienceCode'));
+    $resItems [14] = array("Name" => "MediaCategoryCode", "Value" => $app->request->post('MediaCategoryCode'));
+    $resItems [15] = array("Name" => "PieceCategoryCode", "Value" => $app->request->post('PieceCategoryCode'));
+    $resItems [16] = array("Name" => "PieceCode", "Value" => $app->request->post('PieceCode'));
+    $resItems [17] = array("Name" => "KeyDate", "Value" => $app->request->post('KeyDate'));
+    $resItems [18] = array("Name" => "DirectMailDeliveryStatusCode", "Value" => $app->request->post('DirectMailDeliveryStatusCode'));
+    $resItems [19] = array("Name" => "EmailDeliverabilityStatusCode", "Value" => $app->request->post('EmailDeliverabilityStatusCode'));
+    $resItems [20] = array("Name" => "HasAcceptedLegalTermsCode", "Value" => $app->request->post('HasAcceptedLegalTermsCode'));
+    $resItems [21] = array("Name" => "ProfileCode", "Value" => $app->request->post('ProfileCode'));
+    $responsAttributes = array($resItems);
+
     if ($app->request()->isPost()) {
         $params = Array(
             "LayoutName" => "WebProfileRegistration",
